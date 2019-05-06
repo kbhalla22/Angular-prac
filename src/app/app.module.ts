ac2701c1d2e6d5e67801144d08468ee8982242ef
karanbhalla <kbhalla22@gmail.com>
2019-05-06 11:29:12 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ServersComponent } from './servers/servers.component';
     AppComponent,
     ServerComponent,
   
-    ServersComponent
+    Servers
   ],
   imports: [
     BrowserModule,
